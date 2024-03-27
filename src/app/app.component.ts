913740a78e730c545c71b1d76f65023a0fb520e4
BasicallyClutch <97856451+ColeBaxendale@users.noreply.github.com>
2024-03-26 23:41:05 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
+scrollToElement($event: string) {
+throw new Error('Method not implemented.');
+}
   title = 'frontend';
 }
