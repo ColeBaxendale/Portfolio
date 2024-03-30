d65f9a0b35b88c4e856b95ac6f4ca59c308d646b
Cole Baxendale <cbaxendale99@gmail.com>
2024-03-29 21:02:29 -0400

    "Refactor navbar and add new routes; adjust app routes accordingly"
    This diff includes changes to the `app.routes.ts` file to add new routes and update existing ones, as well as significant modifications to the `nav-bar.component.html` and `nav-bar.component.sass` files to refactor the navigation bar's structure and styling. The new navigation bar features a main section with three items and a sub-main section with multiple categories, each containing several related links. The 'active' state of the links has been adjusted in the SASS file, and a new font has been added for use in the navigation bar.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,20 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent, pathMatch: 'full' },
-    { path: 'about', component: AboutComponent, pathMatch: 'full' },
-    { path: 'projects', component: ProjectsComponent, pathMatch: 'full' },
-    { path: 'contact', component: ContactComponent, pathMatch: 'full' },
+    { path: 'work', component: ProjectsComponent, pathMatch: 'full' },
+    { path: 'ideas', component: ProjectsComponent, pathMatch: 'full' }, //CREATE 
+    { path: 'cv', component: ProjectsComponent, pathMatch: 'full' }, //CREATE 
+    
+    { path: 'full-stack/overhead-doors', component: ContactComponent, pathMatch: 'full' }, //CREATE
+    { path: 'full-stack/stock-simulator', component: ContactComponent, pathMatch: 'full' }, //CREATE
+
+    { path: 'machine-learning/chess', component: ContactComponent, pathMatch: 'full' },
+    { path: 'machine-learning/project', component: ContactComponent, pathMatch: 'full' },
+
+    { path: 'innovations/software1', component: ContactComponent, pathMatch: 'full' },
+    { path: 'innovations/software2', component: ContactComponent, pathMatch: 'full' },
+
+    { path: 'contact/allinfo', component: ContactComponent, pathMatch: 'full' },
+    { path: 'contact/email', component: ContactComponent, pathMatch: 'full' },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
